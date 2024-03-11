38c16379827dad6a5872707a686e0cd9a1f8547f
zestbear <zestbear90@gmail.com>
2024-03-12 02:16:18 +0900

    Kafka에 전송된 데이터들을 earliset 방식으로 읽고 commit하는 예제

---
+++ Kafka-basics/src/main/java/kafka/exercise/ConsumerDemo.java
@@ -0,0 +1,56 @@
+package kafka.exercise;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("The Kafka Consumer");
+
+        String groupId = "my-kafka-application";
+        String topic = "demo-kafka";
+
+        // create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+
+        // create consumer configs
+        properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("value.deserializer", StringDeserializer.class.getName());
+
+        properties.setProperty("group.id", groupId);
+        properties.setProperty("auto.offset.reset", "earliest");
+
+        // create a consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // subscribe to a topic
+        // asList 안에 구독하고 싶은 topic들을 구독하고 싶은 수만큼 넣어도 된다.
+        consumer.subscribe(Arrays.asList(topic));
+
+        // poll for data
+        while (true) {
+            log.info("Polling");
+
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+
+            for (ConsumerRecord<String, String> record : records) {
+                log.info("Key: " + record.key() + ", Value: " + record.value());
+                log.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+            }
+
+        }
+    }
+
+}
